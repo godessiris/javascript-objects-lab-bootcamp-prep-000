8536a2e50ba6fd5569627fb97d0ae1c8f72a49df
Iris Quintero <godessiris@gmail.com>
2019-04-24 03:13:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var newObj = object.assign({}, object);
+  var newObj = Object.assign({}, object);
   delete newObj[key];
   return newObj;
 }
-
 
  function destructivelyDeleteFromObjectByKey(object, key){
    delete object[key];
