eb97a9d3b307e4fcf9a2701adb6eaf0099733807
Iris Quintero <godessiris@gmail.com>
2019-04-24 02:48:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,21 +25,4 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
  }
 
 /*  
-
-  describe('destructivelyDeleteFromObjectByKey(object, key)', function() {
-    it('returns object without the delete key/value pair', function() {
-      var obj = { prop: 1 }
-      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
-      expect(newObj['prop']).toBe(undefined)
-    })
-
-    it('modifies the original object', function() {
-      var obj = { prop: 1 }
-      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
-      expect(obj['prop']).toBe(undefined)
-    })
-  })
-
-})*/
+*/
